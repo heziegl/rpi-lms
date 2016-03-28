84b3a58d102bcaa4ec56cf2893a84587ddd92e51
Heiko Ziegler <git@z5r.de>
2016-03-28 22:04:22 +0200

    timezone settings from host

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 VERSION=7.8.1
+DOCKERHUBUSER=heziegl
 
 sudo docker run -d \
-           	-p 9000:9000 \
-           	-p 3483:3483 \
-           	-p 3483:3483/udp \
+		-p 9000:9000 \
+		-p 3483:3483 \
+		-p 3483:3483/udp \
 		--hostname="docker-lms" \
-           	-v $(pwd)/vol_lms_persist:/var/lib/squeezeboxserver \
-                -v $(pwd)/vol_lms_log:/var/log/squeezeboxserver \
-           	-v $(pwd)/vol_lms_media:/media \
+		-v "/etc/localtime":"/etc/localtime":ro \
+		-v $(pwd)/vol_lms_persist:/var/lib/squeezeboxserver \
+		-v $(pwd)/vol_lms_log:/var/log/squeezeboxserver \
+		-v $(pwd)/vol_lms_media:/media \
 		--name="lms" \
-           	heziegl/rpi-lms:$VERSION
-
-
+		$DOCKERHUBUSER/rpi-lms:$VERSION
