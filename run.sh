9d37ff9a0645ed8fb53e27a86ba7457d86ad18aa
Heiko Ziegler <git@z5r.de>
2016-03-28 17:31:03 +0200

    added hostname

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ sudo docker run -d \
            	-p 9000:9000 \
            	-p 3483:3483 \
            	-p 3483:3483/udp \
+		--hostname="docker-lms" \
            	-v $(pwd)/vol_lms_persist:/var/lib/squeezeboxserver \
                 -v $(pwd)/vol_lms_log:/var/log/squeezeboxserver \
            	-v $(pwd)/vol_lms_media:/media \
