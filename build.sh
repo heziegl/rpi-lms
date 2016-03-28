be10901344339ca23f4148a57c335c7b048d3793
Heiko Ziegler <git@z5r.de>
2016-03-28 21:21:15 +0200

    added push to docker hub

--- build.sh
+++ build.sh
@@ -3,10 +3,13 @@
 
 # lms image tag
 VERSION=7.8.1
+DOCKERHUBUSER=heziegl
 
 # docker image to build
-IMAGE_NAME=heziegl/rpi-lms:$VERSION
+IMAGE_NAME=$DOCKERHUBUSER/rpi-lms:$VERSION
 
 # build grafana runtime
 sudo docker build -t $IMAGE_NAME .
 
+# push to docker hub
+sudo docker push $IMAGE_NAME
